17e1665ac657e1be731556573a131bb14b0d9cac
Thamayanthi-Narayanan <thamayanthinarayanan01@gmail.com>
2025-08-07 13:24:32 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { Users, ChatCircle, PaperPlaneTilt, SignIn } from "phosphor-react";
+import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -23,6 +25,8 @@ const Navbar = () => {
 
   const [isAccountDropdownOpen, setIsAccountDropdownOpen] = useState(false);
 
+  const location =useLocation();
+
   return (
     <>
     <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
@@ -40,7 +44,7 @@ const Navbar = () => {
         {/* desktop */}
 
         <div className="navbar-links">
-          <Link to="/discover">
+          <Link to="/discover" className={location.pathname ==="/discover" ? "active-nav-link" : ""}>
             <span className="icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
                 <path d="M13.5 13.5L17 7l-6.5 3.5L7 17l6.5-3.5z"/>
@@ -48,7 +52,7 @@ const Navbar = () => {
             </span>
             Discover
           </Link>
-          <Link to="/messages">
+          <Link to="/messages" className={location.pathname ==="/messages" ? "active-nav-link" : ""}>
             <span className="icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
@@ -56,14 +60,14 @@ const Navbar = () => {
             </span>
             Messages
           </Link>
-          <a href="#">
+          <Link to="/invites" className={location.pathname ==="/invites" ? "active-nav-link" : ""}>
             <span className="icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
                 <path d="M22 16.92V19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-2.08M16 3.13a4 4 0 0 1 0 7.75M12 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm8 14a8 8 0 0 0-16 0"/>
               </svg>
             </span>
             Invites
-          </a>
+          </Link>
         </div>
 
         <div className="navbar-account" onClick={() => setIsAccountDropdownOpen(! isAccountDropdownOpen)}>
@@ -101,23 +105,23 @@ const Navbar = () => {
 
       <div className={`mobile-menu ${isMobileMenuOpen ? "open" : ""}`}>
         <div className="close-icon" onClick={toggleMenu}>X</div>
-        <a href="#">
+        <Link to="/discover">
             <Users size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Discover
-          </a>
-        <a href="#">
+          </Link>
+        <Link to="/messages">
              <ChatCircle size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Messages
-          </a>
-        <a href="#">
+          </Link>
+        <Link to="#">
                  <PaperPlaneTilt size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Invites
-          </a>
-        <a href="#">
+          </Link>
+        <Link to="/login">
           <SignIn size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Login
-          </a>
-        <a href="#">Sign Up</a>
+          </Link>
+        <Link to="/signup">Sign Up</Link>
       </div>
 
        {isMobileMenuOpen && <div className="menu-overlay" onClick={toggleMenu}></div>}
