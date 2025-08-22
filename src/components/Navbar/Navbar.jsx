3943c51a40447eccf9b94c1e44593d6c1bbb0cc7
bhuvanesh315 <bhuvimuruga01@gmail.com>
2025-08-22 11:27:12 +0530

    linked invite path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { Users, ChatCircle, PaperPlaneTilt, SignIn, UserCircle } from "phosphor-react";
+import {
+  Users,
+  ChatCircle,
+  PaperPlaneTilt,
+  SignIn,
+  UserCircle,
+} from "phosphor-react";
 import { useLocation } from "react-router-dom";
 import { useNavbar } from "./useNavbar";
 
 const Navbar = () => {
-
-  const {
-    isScrolled, isMobileMenuOpen, toggleMenu,location
-  } = useNavbar();
+  const { isScrolled, isMobileMenuOpen, toggleMenu, location } = useNavbar();
 
   // const [isScrolled, setIsScrolled] = useState(false);
 
@@ -32,53 +35,89 @@ const Navbar = () => {
 
   // const location =useLocation();
 
-
-
   return (
     <>
-    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
-      <div className="navbar-left">
-        <div className="navbar-logo">CoffeeSpace</div>
+      <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
+        <div className="navbar-left">
+          <div className="navbar-logo">CoffeeSpace</div>
 
-     {/* Mobile Icon */}
+          {/* Mobile Icon */}
 
-       {/* <div className="mobile-menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {/* <div className="mobile-menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <svg width="28" height="28" fill="none" stroke="#2f2121" strokeWidth="2" viewBox="0 0 24 24">
             <path d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </div>  */}
 
-        {/* desktop */}
+          {/* desktop */}
 
-        <div className="navbar-links">
-          <Link to="/discover" className={location.pathname ==="/discover" ? "active-nav-link" : ""}>
-            <span className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
-                <path d="M13.5 13.5L17 7l-6.5 3.5L7 17l6.5-3.5z"/>
-              </svg>
-            </span>
-            Discover
-          </Link>
-          <Link to="/messages" className={location.pathname ==="/messages" ? "active-nav-link" : ""}>
-            <span className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
-                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
-              </svg>
-            </span>
-            Messages
-          </Link>
-          <Link to="/invites" className={location.pathname ==="/invites" ? "active-nav-link" : ""}>
-            <span className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#8a6969" strokeWidth="2" viewBox="0 0 24 24">
-                <path d="M22 16.92V19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-2.08M16 3.13a4 4 0 0 1 0 7.75M12 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm8 14a8 8 0 0 0-16 0"/>
-              </svg>
-            </span>
-            Invites
-          </Link>
-        
-        </div>
+          <div className="navbar-links">
+            <Link
+              to="/discover"
+              className={
+                location.pathname === "/discover" ? "active-nav-link" : ""
+              }
+            >
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="none"
+                  stroke="#8a6969"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.5 13.5L17 7l-6.5 3.5L7 17l6.5-3.5z" />
+                </svg>
+              </span>
+              Discover
+            </Link>
+            <Link
+              to="/messages"
+              className={
+                location.pathname === "/messages" ? "active-nav-link" : ""
+              }
+            >
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="none"
+                  stroke="#8a6969"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+                </svg>
+              </span>
+              Messages
+            </Link>
+            <Link
+              to="/invites"
+              className={
+                location.pathname === "/invites" ? "active-nav-link" : ""
+              }
+            >
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="none"
+                  stroke="#8a6969"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 16.92V19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-2.08M16 3.13a4 4 0 0 1 0 7.75M12 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm8 14a8 8 0 0 0-16 0" />
+                </svg>
+              </span>
+              Invites
+            </Link>
+          </div>
 
-        {/* <div className="navbar-account" onClick={() => setIsAccountDropdownOpen(! isAccountDropdownOpen)}>
+          {/* <div className="navbar-account" onClick={() => setIsAccountDropdownOpen(! isAccountDropdownOpen)}>
           <span className="icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="#2f2121" strokeWidth="2" viewBox="0 0 24 24">
               <path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0 2c-4.418 0-8 2.015-8 4.5V21h16v-2.5c0-2.485-3.582-4.5-8-4.5Z"/>
@@ -100,47 +139,67 @@ const Navbar = () => {
 
         </div> */}
 
-        <Link to="/get-started"
-        className={`navbar-account ${location.pathname === "/get-started" ? "active-nav-link" : ""}`}
-        >
-          <UserCircle size={24} />
-          Get Started
+          <Link
+            to="/get-started"
+            className={`navbar-account ${
+              location.pathname === "/get-started" ? "active-nav-link" : ""
+            }`}
+          >
+            <UserCircle size={24} />
+            Get Started
           </Link>
 
           <div className="mobile-menu-icon" onClick={toggleMenu}>
-            <svg width="28" height="28" fill="none" stroke="#2f2121" strokeWidth="2" viewBox="0 0 24 24">
+            <svg
+              width="28"
+              height="28"
+              fill="none"
+              stroke="#2f2121"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
               <path d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </div>
-      </div>
-    </nav>
+        </div>
+      </nav>
 
-    
       {/* mobile menu */}
 
       <div className={`mobile-menu ${isMobileMenuOpen ? "open" : ""}`}>
-        <div className="close-icon" onClick={toggleMenu}>X</div>
+        <div className="close-icon" onClick={toggleMenu}>
+          X
+        </div>
         <Link to="/discover">
-            <Users size={22} color="#8a6969" style={{ marginRight: "8px" }} />
+          <Users size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Discover
-          </Link>
+        </Link>
         <Link to="/messages">
-             <ChatCircle size={22} color="#8a6969" style={{ marginRight: "8px" }} />
+          <ChatCircle
+            size={22}
+            color="#8a6969"
+            style={{ marginRight: "8px" }}
+          />
           Messages
-          </Link>
-        <Link to="#">
-                 <PaperPlaneTilt size={22} color="#8a6969" style={{ marginRight: "8px" }} />
+        </Link>
+        <Link to="/invites">
+          <PaperPlaneTilt
+            size={22}
+            color="#8a6969"
+            style={{ marginRight: "8px" }}
+          />
           Invites
-          </Link>
+        </Link>
         <Link to="/login">
           <SignIn size={22} color="#8a6969" style={{ marginRight: "8px" }} />
           Login
-          </Link>
+        </Link>
         <Link to="/signup">Sign Up</Link>
       </div>
 
-       {isMobileMenuOpen && <div className="menu-overlay" onClick={toggleMenu}></div>}
-
+      {isMobileMenuOpen && (
+        <div className="menu-overlay" onClick={toggleMenu}></div>
+      )}
     </>
   );
 };
